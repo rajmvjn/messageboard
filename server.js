e8db28d693643f88379aaff3a60a7c069359e66c
Rajesh Vijayan <A-8076@ibsplc.com>
2018-05-28 16:41:50 +0530

    added router

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
 var messages = [{text:'some text', owner:'Tim'}, {text:'other text', owner:'Raj'}]
 
-app.get('/messages', (req, res) => {
+app.use(bodyParser.json())
+
+app.use( ( req, res, next ) => {
+    res.header("Acces-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+var api = express.Router();
+
+api.get('/messages', ( req, res ) => {
     res.send(messages);
 })
+
+api.post('/messages', ( req, res ) => {
+    messages.push(req.body);
+    res.sendStatus(200);
+})
+
+app.use('/api', api);
 
 app.listen(1234)
